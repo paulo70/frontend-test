2117fe4e105d7241225a476286863ce69dbf5520
paulo <paulofrontend@gmail.com>
2017-10-09 08:17:59 -0300

    delete files jasmine

--- public/jasmine/test.spec.js
+++
@@ -1,8 +0,0 @@
-import * as test from './test';
-
-describe('tests', function() {
-  it('check if the parameter is valid', function() {
-    let obj;
-    expect(test.isRequestOK(obj)).toEqual(4)
-  });
-});
